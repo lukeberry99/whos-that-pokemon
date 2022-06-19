b42750f080285b9c54f3823043fc6f93d55c4440
Luke Berry <luke@spill.chat>
2022-06-19 16:21:32 +0100

    Better number gen

--- src/utils/get-random-pokemon-id.ts
+++ src/utils/get-random-pokemon-id.ts
@@ -1,7 +1,7 @@
 const MAX_ID = 151
 
 const getRandomPokemonId = (seenList: number[]): number => {
-  const randomId = Math.floor(Math.random() * MAX_ID + 1 - 1)
+  const randomId = Math.floor((Math.random() * MAX_ID) + 1)
 
   if (seenList.includes(randomId)) {
     return getRandomPokemonId(seenList)
